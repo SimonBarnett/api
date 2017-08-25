9f85c8ff08bdbaa35e0995ac0de27dc5736e3df6
Simon Barnett <simonbarnett@emerge-it.co.uk>
2017-08-25 10:01:31 +0100

    remove temp sql files

--- setLog.sql
+++
@@ -1,5 +0,0 @@
-USE [system]
-update [dbo].[apiLog] set 
-[severity] = '{2}',
-[logText] = '{1}'
-where BubbleID = '{0}'
--- setXML.sql
+++
@@ -1,4 +0,0 @@
-USE [system]
-update [dbo].[apiLog] set 
-[logXml] = '{1}'
-where BubbleID = '{0}'
