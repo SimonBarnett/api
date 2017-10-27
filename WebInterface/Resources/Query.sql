d269c0dbf706a7aa724057ccb5c07ab0c1cc758b
Simon Barnett <simonbarnett@emerge-it.co.uk>
2017-10-27 13:55:57 +0100

    api.zip

--- WebInterface/Resources/Query.sql
+++ WebInterface/Resources/Query.sql
@@ -12,27 +12,41 @@ set @dd = datepart(day, getdate())
 set @mm = datepart(MONTH, getdate())
 set @yy = datepart(year, getdate())
 
+declare @Row bigint
+set @Row = 0
+
+declare @tab as table(
+	[row] bigint,
+	[displayTime] [varchar](8), 
+	[BubbleID] varchar(64),
+	[severity] int	
+)
+
+INSERT INTO @tab
+SELECT 
+	rank() over (order by endpoint, logDate, logTime) as [row]
+	,[displayTime] as [displayTime]
+	,[BubbleID] as [BubbleID]
+	,[severity] as [severity]				  
+	FROM [system].[dbo].[apiLog]
+	where 0=0
+		and [logYear] = @yy
+		and [logMonth] = @mm
+		and [logDay]= @dd
+		and lower([environment]) = lower(@environment)
+		and lower([endpoint]) = lower(@page)
+
+	order by logTime
+
 select '', (
 	SELECT 
-		[displayTime] as "@Time"
-		  ,[BubbleID] as "@BubbleID"
-      
-		  --,[method]
-		  --,[endpoint]
-		  ,[severity] as "@Severity"
-		  ,[fKey] as "@fkey"
-		  --,[logXml]
-		  --,[logText]
+		[row] as "@row"
+		,[displayTime] as "@Time"
+		,[BubbleID] as "@BubbleID"
+		,[severity] as "@Severity"			
+	FROM @tab
+	where 0=0
+		and row > @Row	  
+	for XML PATH('item'), type)
 
-
-	  FROM [system].[dbo].[apiLog]
-	  where 0=0
-		  and [logYear] = @yy
-		  and [logMonth] = @mm
-		  and [logDay]= @dd
-		  and lower([environment]) = lower(@environment)
-		  and lower([endpoint]) = lower(@page)
-
-		order by logTime DESC
-		for XML PATH('item'), type)
 for XML PATH('log'), type
